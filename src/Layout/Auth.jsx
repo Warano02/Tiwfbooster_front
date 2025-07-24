b75eeedb8bc1d738f781c71c6305923bd0558b16
Warano02 <carineteoi@gmail.com>
2025-07-24 17:22:15 +0100

    comming back

--- src/Layout/Auth.jsx
+++ src/Layout/Auth.jsx
@@ -1,24 +1,10 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 // import { useAppContext } from "../context/AppContext";
 // import { useEffect } from "react";
-import Header from "../components/auth/Header";
-
 function Auth() {
-    // const navigate = useNavigate()
-    // const { user } = useAppContext()
-    // useEffect(() => {
-    //     if (user) {
-    //         alert("User is login")
-    //         navigate("/")
-    //     } else {
-    //         alert("You Can Login")
-    //     }
-    // })
-    const location = useLocation()
-
+ 
     return (
         <>
-            {location.pathname !== "/auth/" && <Header />}
             <div className="w-full h-full">
                 <Outlet />
             </div>
